e0f92668ae449e0d932bb65c4e24ca8557a99fac
Olivier Huber <huber@wias-berlin.de>
2020-05-17 11:58:28 +0200

    add Artifact.toml generator

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # julia-utils
 Some utils used for julia tasks
+
+
+- `artifact_generator.jl`: from artifact archives, build the Artifact.toml file.
+  This is useful for using artifact for binary package not built via `BinaryBuilder.jl`
---
+++ artifact_generator.jl
@@ -0,0 +1,39 @@
+using Pkg
+using Pkg.Artifacts
+import Pkg.BinaryPlatforms
+using SHA
+
+###########################################################
+# Start of config
+###########################################################
+name = "ReSHOP"
+version = "0.1.0"
+
+url = "https://nullptr.fr/julia/"
+files = readdir("/tmp/ReSHOP_jll", join=true)
+artifact_toml = "/tmp/$(name)_jll-artifact"
+
+prefix = "$name.v$(version)"
+postfix = "tar.gz"
+
+
+###########################################################
+# End of config
+###########################################################
+
+
+for f in files
+	f_hash = create_artifact() do artifact_path
+		run(`tar xvf $(f) -C $(artifact_path)`)
+	end
+
+	d_hash = ""
+	open(f) do ff
+		d_hash = bytes2hex(sha2_256(ff))
+	end
+	triplet_str = replace(replace(basename(f), "."*postfix => ""), prefix*"." => "")
+	plat = Pkg.BinaryPlatforms.platform_key_abi(triplet_str)
+
+	bind_artifact!(artifact_toml, "ReSHOP", f_hash, platform=plat, download_info=[(url*basename(f), d_hash)])
+end
+
